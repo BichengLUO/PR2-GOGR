2570fc35ff1e5d3a85c70d4064725f17364913c8
BichengLUO <nobodycrackme@gmail.com>
2018-04-13 17:32:41 -0400

    First commit of Webots project; add a kinect on PR2

---
+++ controllers/pr2_controller/build/release/pr2_controller.d
@@ -0,0 +1,6 @@
+build/release/pr2_controller.o: pr2_controller.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/include/controller/c/webots/camera_recognition_object.h
